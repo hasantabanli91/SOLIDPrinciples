06fe830caeea80b85b3afe4ae7abb497039ac63e
hasan.tabanli <hasan.tabanli@06GZLSB-0010>
2023-01-26 15:06:25 +0300

    Single Responsibility

---
+++ SOLID/1-SingleResponsibility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SOLID
+{
+    public class SingleResponsibility
+    {
+        public void SingleResponseInCafe()
+        {
+            string order = OrderService.CreateOrder();
+            Console.WriteLine(order);
+
+            bool paymentSituation = PaymentService.MakePayment();
+            if (paymentSituation)
+            {
+                Console.WriteLine("    Payment received");
+            }
+        }
+    }
+    public static class OrderService
+    {
+        public static string CreateOrder()
+        {
+            string Order = "    Order is getting ready";
+            //Code to Create Order
+            return Order;
+        }
+    }
+    public static class PaymentService
+    {
+        public static bool MakePayment()
+        {
+            //Code to Make Payment
+            return true;
+        }
+    }
+}
--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Solid Principles are very popular design principles in the object-oriented programming world and
+// developers try to use these principles in their code with the intention of adding flexibility and maintainability to the code i.e. to write better software.
+// They are very important for flexible, reliable & robust software development.
+
+using SOLID;
+
+Console.WriteLine("");
+Console.WriteLine("    Solid Principles");
+Console.WriteLine("");
+Console.WriteLine("    1 - Single Responsibility");
+Console.WriteLine("        > Its means each software module or a class should have one and only one reason to change");
+SingleResponsibility singleResponsibility = new SingleResponsibility();
+singleResponsibility.SingleResponseInCafe();
